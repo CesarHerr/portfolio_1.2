d8742ac4a7552b1e3d8c96495c051099b123225c
CesarHerr <cesarherr321@gmail.com>
2025-02-17 17:02:21 -0300

    Add experience component with locales

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux'
 import './App.css'
-import Navigate from './components/Navigate';
-import Projects from './components/Projects';
-import Skills from './components/Skills';
-import ContactForm from './components/ContactForm';
-import Popup from './components/Popup';
-import About from './components/About';
+import Navigate from './components/Navigate'
+import Projects from './components/Projects'
+import Skills from './components/Skills'
+import ContactForm from './components/ContactForm'
+import Popup from './components/Popup'
+import About from './components/About'
+import Experience from './components/Experience'
 
 function App() {
-  const { project } = useSelector((state) => state.project);
+  const { project } = useSelector(state => state.project)
 
   return (
-    <main className="flex flex-col mx-auto bg-myGreen text-myGray dark:bg-slate-900 dark:text-white">
+    <main className='flex flex-col mx-auto bg-myGreen text-myGray dark:bg-slate-900 dark:text-white'>
       <About />
-      <Navigate />      
+      <Navigate />
+      <Experience />
       <Projects />
       <Skills />
       <ContactForm />
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ function About() {
   const lenguaje = i18n.language
   return (
     <section
-      className='h-screen relative hidden items-end gap-2 pr-4 md:flex flex-col xl:pr-20
+      className='h-screen relative hidden items-end gap-2 xl:gap-4 pr-4 md:flex flex-col xl:pr-20
       md:ml-[20rem] xl:ml-[25rem] mb-24 custom-opacity'
       id='about'>
       {lenguaje === 'es' && (
@@ -28,10 +28,10 @@ function About() {
           />
         </button>
       )}
-      <h3 className='w-10/12 text-left text-3xl lg:text-4xl xl:text-5xl mt-[20%] text-myBlue dark:text-myLightBlue'>
+      <h3 className='w-11/12 text-left text-3xl lg:text-4xl xl:text-5xl mt-[15%] text-myBlue dark:text-myLightBlue'>
         {t('about.developer')}
       </h3>
-      <p className='w-10/12 text-left text-xl xl:text-2xl'> {t('about.me')}</p>
+      <p className='w-11/12 text-left text-xl xl:text-2xl'> {t('about.me')}</p>
       <div className='w-[220px] h-[260px] opacity-80 mt-26 relative lg:top-5 right-10'>
         <img
           src={images.post}
